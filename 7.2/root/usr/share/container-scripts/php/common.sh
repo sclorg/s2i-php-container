c7b49c870e00e9eeffab7f0c004712081dc410fd
Samyunodos <samyunodos@gmail.com>
2019-10-13 23:43:48 +0200

    all

--- 7.2/root/usr/share/container-scripts/php/common.sh
+++ 7.2/root/usr/share/container-scripts/php/common.sh
@@ -45,7 +45,7 @@ config_fpm() {
 
     echo "php_value[session.save_path]    = /tmp/session" >> /etc/opt/rh/rh-php72/php-fpm.d/www.conf
     echo " php_value[soap.wsdl_cache_dir]  = /tmp/wsdlcache" >> /etc/opt/rh/rh-php72/php-fpm.d/www.conf
-    echo "php_value[opcache.file_cache]  = /tmp/opcache" >> /etc/opt/rh/rh-php72/php-fpm.d/www.conf
+  #  echo "php_value[opcache.file_cache]  = /tmp/opcache" >> /etc/opt/rh/rh-php72/php-fpm.d/www.conf
 
 
 }
