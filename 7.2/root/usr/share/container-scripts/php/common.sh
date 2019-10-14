26eff85162a342cba9ef28d8834959a1332898a7
Samyunodos <samyunodos@gmail.com>
2019-10-14 07:07:59 +0200

    all

--- 7.2/root/usr/share/container-scripts/php/common.sh
+++ 7.2/root/usr/share/container-scripts/php/common.sh
@@ -36,13 +36,13 @@ config_fpm() {
     echo "</FilesMatch>" >> /etc/httpd/conf.d/rh-php72-php.conf
     sed -i '/listen.allowed_clients/s/^/;/g' /etc/opt/rh/rh-php72/php-fpm.d/www.conf
     
-    sed -i '/^.*session.save_path.*$/d' /etc/opt/rh/rh-php72/php-fpm.d/www.conf
-    sed -i '/^.*soap.wsdl_cache_dir.*$/d' /etc/opt/rh/rh-php72/php-fpm.d/www.conf
-    sed -i '/^.*opcache.file_cache.*$/d' /etc/opt/rh/rh-php72/php-fpm.d/www.conf
+    # sed -i '/^.*session.save_path.*$/d' /etc/opt/rh/rh-php72/php-fpm.d/www.conf
+    # sed -i '/^.*soap.wsdl_cache_dir.*$/d' /etc/opt/rh/rh-php72/php-fpm.d/www.conf
+    # sed -i '/^.*opcache.file_cache.*$/d' /etc/opt/rh/rh-php72/php-fpm.d/www.conf
 
-    echo "php_value[session.save_path]    = /tmp/session" >> /etc/opt/rh/rh-php72/php-fpm.d/www.conf
-    echo " php_value[soap.wsdl_cache_dir]  = /tmp/wsdlcache" >> /etc/opt/rh/rh-php72/php-fpm.d/www.conf
-    echo "php_value[opcache.file_cache]  = /tmp/opcache" >> /etc/opt/rh/rh-php72/php-fpm.d/www.conf
+    # echo "php_value[session.save_path]    = /tmp/session" >> /etc/opt/rh/rh-php72/php-fpm.d/www.conf
+    # echo " php_value[soap.wsdl_cache_dir]  = /tmp/wsdlcache" >> /etc/opt/rh/rh-php72/php-fpm.d/www.conf
+    # echo "php_value[opcache.file_cache]  = /tmp/opcache" >> /etc/opt/rh/rh-php72/php-fpm.d/www.conf
 
 
 }
