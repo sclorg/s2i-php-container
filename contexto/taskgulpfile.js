b896c25a781e5667604e81cbb17161eb0eba9ab2
Samyunodos <samyunodos@gmail.com>
2019-10-31 13:47:58 +0100

    all

--- contexto/taskgulpfile.js
+++ contexto/taskgulpfile.js
@@ -6,32 +6,78 @@ const fs = require('fs-extra');
 
 
 
-const srcpath = '/opt/app-root/src/wp-content';
-const dstpath = '/silo/wordpress';
+const srcwpcontent = '/opt/app-root/src/wp-content';
+const dstwpcontent = '/silo/wordpress';
+const srcwphtaccess = '/opt/app-root/src/.httaccess';
+const dstwphtaccess = '/silo/wordpress/.htaccess';
+const srcwpconfig = '/opt/app-root/src/wp-config.php' ;
+const dstwpconfig = '/silo/wordpress/wp-config.php';
+
 
 // Async/Await:
-async function moverwpcontent (srcpath,dstpath) {
+async function moverwpcontent () {
   try {
-    await fs.copy( srcpath, dstpath, {overwrite: true});
+    await fs.move( srcwpcontent, dstwpcontent, {overwrite: true});
     console.log('success!');
   } catch (err) {
     console.error(err);
   }
 }
+
+// Async/Await:
+async function moverwpconfig () {
+  try {
+    await fs.move( srcwpconfig, dstwpconfig, {overwrite: true});
+    console.log('success!');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Async/Await:
+async function moverwphtcaccess () {
+  try {
+    await fs.move( srcwphtaccess, dstwphtaccess, {overwrite: true});
+    console.log('success!');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
  
 
-async function crearlinkwpcontent (srcpath, dstpath) {
+async function crearlinkwpconfig () {
   try {
-    await fs.ensureSymlink(srcpath, dstpath);
+    await fs.ensureSymlink(dstwpconfig, srcwpconfig);
     console.log('success!');
   } catch (err) {
     console.error(err);
   }
 }
 
-gulp.task('mover', function() {
-  gulp.series('moverwpcontent', 'crearlinkwpcontent');
-  
+async function crearlinkwphtcaccess () {
+  try {
+    await fs.ensureSymlink(dstwphtaccess, srcwphtaccess);
+    console.log('success!');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+
+
+gulp.task('tmoverwpcontent', function() {
+  gulp.series('moverwpcontent');
+});
+
+gulp.task('tmoverwpconfig', function() {
+  gulp.series('moverwpconfig', 'crearlinkwpconfig');
+});
+
+gulp.task('tmoverwphtcaccess', function() {
+  gulp.series('moverwphtcaccess', 'crearlinkwphttcacces');
 });
 
 gulp.task('watch',  function() {
