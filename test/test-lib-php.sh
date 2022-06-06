0a4b765f180affaa0c7fd82ee6440c664cd20f16
Zuzana Miklankova <zmiklank@redhat.com>
2022-06-06 13:36:16 +0200

    run all openshift4 tests even when one fails

--- test/test-lib-php.sh
+++ test/test-lib-php.sh
@@ -12,8 +12,7 @@ source ${THISDIR}/test-lib.sh
 source ${THISDIR}/test-lib-openshift.sh
 
 function test_php_integration() {
-  local image_name=$1
-  ct_os_test_s2i_app "${image_name}" \
+  ct_os_test_s2i_app "${IMAGE_NAME}" \
                      "https://github.com/sclorg/s2i-php-container.git" \
                      "test/test-app" \
                      "Test PHP passed"
