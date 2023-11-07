e2e3acafc695ab11791e9187484663c712e8b8d3
Petr "Stone" Hracek <phracek@redhat.com>
2023-11-07 12:31:01 +0100

    Add test case for cakephp-ex template
    Supported test scenarios are only
    RHEL8 and VERSION=7.4
    RHEL7 and VERSION=7.3
    
    The rest will be solved by issue
    https://github.com/sclorg/cakephp-ex/issues/136
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test-lib-php.sh
+++ test/test-lib-php.sh
@@ -27,4 +27,23 @@ function test_php_imagestream() {
                               "Test PHP passed"
 }
 
+# Check the template
+function test_php_template() {
+  local supported_use_case
+  if [ "${OS}" == "rhel8" ] && [ "${VERSION}" == "7.4" ]; then
+    supported_use_case="True"
+  fi
+  if [ "${OS}" == "rhel7" ] && [ "${VERSION}" == "7.3" ]; then
+    supported_use_case="True"
+  fi
+  if [ "${supported_use_case:-False}" == "True" ]; then
+    BRANCH_TO_TEST="master"
+    ct_os_test_template_app "${IMAGE_NAME}" \
+                        https://raw.githubusercontent.com/sclorg/cakephp-ex/${BRANCH_TO_TEST}/openshift/templates/cakephp.json \
+                        php \
+                        'Welcome to your CakePHP application on OpenShift' \
+                        8080 http 200 "-p SOURCE_REPOSITORY_REF=${BRANCH_TO_TEST} -p SOURCE_REPOSITORY_URL=https://github.com/sclorg/cakephp-ex.git -p PHP_VERSION=${VERSION} -p NAME=php-testing"
+  fi
+}
+
 # vim: set tabstop=2:shiftwidth=2:expandtab:
