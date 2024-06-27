f33401057c906326e624f4724fa88dbd1aab0b16
Petr "Stone" Hracek <phracek@redhat.com>
2024-06-27 10:40:13 +0200

    Update deploy_templates
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_deploy_templates.py
+++ test/test_deploy_templates.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+import pytest
 from container_ci_suite.openshift import OpenShiftAPI
 from container_ci_suite.utils import check_variables
 
@@ -23,32 +24,55 @@ else:
     branch_to_test = "master"
     check_msg = "Welcome to your CakePHP application on OpenShift"
 
+TAGS = {
+    "rhel8": "-ubi8",
+    "rhel9": "-ubi9"
+}
+TAG = TAGS.get(OS, None)
+
+DEPLOYED_MYSQL_IMAGE = "quay.io/sclorg/mysql-80-c8s"
+IMAGE_SHORT = f"mysql:8.0-el8"
+IMAGE_TAG = f"8.0-el8"
+
 
 class TestDeployTemplate:
 
     def setup_method(self):
         self.oc_api = OpenShiftAPI(pod_name_prefix="php-testing", version=VERSION)
         self.oc_api.import_is("imagestreams/php-rhel.json", "", skip_check=True)
+        assert self.oc_api.upload_image(DEPLOYED_MYSQL_IMAGE, IMAGE_SHORT)
 
     def teardown_method(self):
         self.oc_api.delete_project()
 
-    def test_php_template_inside_cluster(self):
+    @pytest.mark.parametrize(
+        "template",
+        [
+            "cakephp.json",
+        ]
+    )
+    def test_php_template_inside_cluster(self, template):
 
         service_name = "php-testing"
         template_url = self.oc_api.get_raw_url_for_json(
-            container="cakephp-ex", dir="openshift/templates", filename="cakephp.json", branch=branch_to_test
+            container="cakephp-ex", dir="openshift/templates", filename=template, branch=branch_to_test
         )
+        openshift_args = [
+            f"SOURCE_REPOSITORY_REF={branch_to_test}",
+            f"PHP_VERSION={VERSION}{TAG}",
+            f"NAME={service_name}"
+        ]
+        if template != "cakephp.json":
+            openshift_args.extend([
+                f"MYSQL_VERSION={IMAGE_TAG}",
+                f"DATABASE_USER=testu",
+                f"DATABASE_PASSWORD=testp"
+            ])
         assert self.oc_api.deploy_template_with_image(
             image_name=IMAGE_NAME,
             template=template_url,
             name_in_template="php",
-            openshift_args=[
-                f"SOURCE_REPOSITORY_REF={branch_to_test}",
-                f"PHP_VERSION={VERSION}",
-                f"SOURCE_REPOSITORY_URL=https://github.com/sclorg/cakephp-ex.git",
-                f"NAME={service_name}"
-            ]
+            openshift_args=openshift_args
         )
         assert self.oc_api.template_deployed(name_in_template=service_name)
         assert self.oc_api.check_response_inside_cluster(
