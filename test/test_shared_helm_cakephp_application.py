8feaea495f73fed1bb52c13016ca97acb12a1081
Petr "Stone" Hracek <phracek@redhat.com>
2025-05-15 09:46:26 +0200

    Add new imagestreams for RHEL10 and RHEL9.6.
    Update all OpenShift 4 and OpenShift 4 Pytest test suites.
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_shared_helm_cakephp_application.py
+++ test/test_shared_helm_cakephp_application.py
@@ -7,6 +7,8 @@ from pathlib import Path
 from container_ci_suite.helm import HelmChartsAPI
 from container_ci_suite.utils import check_variables
 
+from constants import TAGS
+
 if not check_variables():
     print("At least one variable from IMAGE_NAME, OS, VERSION is missing.")
     sys.exit(1)
@@ -26,11 +28,7 @@ elif VERSION == "8.1" or VERSION == "8.2" or VERSION == "8.3":
 else:
     check_msg = "Welcome to your CakePHP application on OpenShift"
 
-TAGS = {
-    "rhel8": "-ubi8",
-    "rhel9": "-ubi9"
-}
-TAG = TAGS.get(OS, None)
+TAG = TAGS.get(OS)
 
 
 class TestHelmCakePHPTemplate:
@@ -47,43 +45,7 @@ class TestHelmCakePHPTemplate:
     def teardown_method(self):
         self.hc_api.delete_project()
 
-    def test_curl_connection(self):
-        if OS == "rhel10":
-            pytest.skip("Do NOT test on RHEL10 yet.")
-        if VERSION == "8.3":
-            pytest.skip("Do NOT test on version 8.3 yet.")
-
-        branch_to_test = "4.X"
-        check_msg = "Welcome to CakePHP"
-        if self.hc_api.shared_cluster:
-            pytest.skip("Do NOT test on shared cluster")
-        if VERSION.startswith("8.2") or VERSION.startswith("8.3"):
-            branch_to_test = "5.X"
-            check_msg = "Welcome to CakePHP"
-        self.hc_api.package_name = "redhat-php-imagestreams"
-        assert self.hc_api.helm_package()
-        assert self.hc_api.helm_installation()
-        self.hc_api.package_name = "redhat-php-cakephp-application"
-        assert self.hc_api.helm_package()
-        assert self.hc_api.helm_installation(
-            values={
-                "php_version": f"{VERSION}{TAG}",
-                "namespace": self.hc_api.namespace,
-                "source_repository_ref": branch_to_test,
-                "name": "cakephp-example"
-            }
-        )
-        assert self.hc_api.is_s2i_pod_running(pod_name_prefix="cakephp-example", timeout=480)
-        assert self.hc_api.oc_api.check_response_inside_cluster(
-            name_in_template="cakephp-example",
-            expected_output=check_msg,
-        )
-
     def test_by_helm_test(self):
-        if OS == "rhel10":
-            pytest.skip("Do NOT test on RHEL10 yet.")
-        if VERSION == "8.3":
-            pytest.skip("Do NOT test on version 8.3 yet.")
         branch_to_test = "4.X"
         check_msg = "Welcome to CakePHP"
         if VERSION.startswith("8.2") or VERSION.startswith("8.3"):
