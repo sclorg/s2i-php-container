0afff87d3e5eb4bf0dcb113ae40dd2b32c74b511
Petr "Stone" Hracek <phracek@redhat.com>
2025-10-15 10:34:19 +0200

    Rename `test_php_*.py` to `test_ocp_*.py`
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ test/test_ocp_php_deploy_templates.py
@@ -0,0 +1,47 @@
+import pytest
+from container_ci_suite.openshift import OpenShiftAPI
+
+from conftest import VARS, TAGS
+
+
+class TestDeployTemplate:
+
+    def setup_method(self):
+        self.oc_api = OpenShiftAPI(pod_name_prefix=f"php-{VARS.SHORT_VERSION}-testing", version=VARS.VERSION)
+
+    def teardown_method(self):
+        self.oc_api.delete_project()
+
+    @pytest.mark.parametrize(
+        "template",
+        [
+            "cakephp.json",
+        ]
+    )
+    def test_php_template_inside_cluster(self, template):
+        self.oc_api.import_is("imagestreams/php-rhel.json", "", skip_check=True)
+        service_name = f"php-{VARS.SHORT_VERSION}-testing"
+        template_url = self.oc_api.get_raw_url_for_json(
+            container="cakephp-ex", dir="openshift/templates", filename=template, branch=VARS.BRANCH_TO_TEST
+        )
+        openshift_args = [
+            f"SOURCE_REPOSITORY_REF={VARS.BRANCH_TO_TEST}",
+            f"PHP_VERSION={VARS.SHORT_VERSION}{TAGS.get(VARS.OS)}",
+            f"NAME={service_name}"
+        ]
+        # if template != "cakephp.json":
+        #     openshift_args.extend([
+        #         f"MYSQL_VERSION={IMAGE_TAG}",
+        #         f"DATABASE_USER=testu",
+        #         f"DATABASE_PASSWORD=testp"
+        #     ])
+        assert self.oc_api.deploy_template_with_image(
+            image_name=VARS.IMAGE_NAME,
+            template=template_url,
+            name_in_template="php",
+            openshift_args=openshift_args
+        )
+        assert self.oc_api.is_template_deployed(name_in_template=service_name)
+        assert self.oc_api.check_response_inside_cluster(
+            name_in_template=service_name, expected_output=VARS.CHECK_MSG
+        )
---
+++ test/test_ocp_shared_helm_chart_imagestreams.py
@@ -0,0 +1,39 @@
+import pytest
+
+from container_ci_suite.helm import HelmChartsAPI
+
+from conftest import VARS
+
+
+class TestHelmRHELPHPImageStreams:
+
+    def setup_method(self):
+        package_name = "redhat-php-imagestreams"
+        self.hc_api = HelmChartsAPI(
+            path=VARS.TEST_DIR, package_name=package_name, tarball_dir=VARS.TEST_DIR, shared_cluster=True
+        )
+        self.hc_api.clone_helm_chart_repo(
+            repo_url="https://github.com/sclorg/helm-charts", repo_name="helm-charts",
+            subdir="charts/redhat"
+        )
+
+    def teardown_method(self):
+        self.hc_api.delete_project()
+
+    @pytest.mark.parametrize(
+        "version,registry,expected",
+        [
+            ("8.3-ubi10", "registry.redhat.io/ubi10/php-83:latest", True),
+            ("8.3-ubi9", "registry.redhat.io/ubi9/php-83:latest", True),
+            ("8.2-ubi9", "registry.redhat.io/ubi9/php-82:latest", True),
+            ("8.2-ubi8", "registry.redhat.io/ubi8/php-82:latest", True),
+            ("8.1-ubi9", "registry.redhat.io/ubi9/php-81:latest", False),
+            ("8.0-ubi9", "registry.redhat.io/ubi9/php-80:latest", True),
+            ("8.0-ubi8", "registry.redhat.io/ubi8/php-80:latest", False),
+            ("7.4-ubi8", "registry.redhat.io/ubi8/php-74:latest", True),
+        ],
+    )
+    def test_package_imagestream(self, version, registry, expected):
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        assert self.hc_api.check_imagestreams(version=version, registry=registry) == expected
