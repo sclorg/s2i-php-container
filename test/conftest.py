728c045fb31f601c52e0f379d9fd9c2799276d14
Petr "Stone" Hracek <phracek@redhat.com>
2025-10-23 17:03:41 +0200

    Remove obsoleted variables that are not used at all.
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/conftest.py
+++ test/conftest.py
@@ -16,37 +16,26 @@ TAGS = {
     "rhel10": "-ubi10",
 }
 
-MYSQL_TAGS = {
-    "rhel8": "-el8",
-    "rhel9": "-el9",
-    "rhel10": "-el10",
-}
-
 Vars = namedtuple(
     "Vars", [
-        "OS", "TAG", "MYSQL_VERSION", "VERSION", "IMAGE_NAME", "IS_MINIMAL",
+        "OS", "TAG", "VERSION", "IMAGE_NAME", "IS_MINIMAL",
         "SHORT_VERSION", "TEST_DIR", "BRANCH_TO_TEST", "CHECK_MSG"
     ]
 )
 VERSION = os.getenv("VERSION")
 OS = os.getenv("TARGET").lower()
-BRANCH_TO_TEST = "master"
-CHECK_MSG = "Welcome to your CakePHP"
 
 VARS = Vars(
     OS=OS,
     TAG=TAGS.get(OS),
-    MYSQL_VERSION=f"8.0{MYSQL_TAGS.get(OS)}",
     VERSION=VERSION,
     IMAGE_NAME=os.getenv("IMAGE_NAME"),
     IS_MINIMAL="minimal" in VERSION,
     SHORT_VERSION=VERSION.replace(".", ""),
     TEST_DIR=Path(__file__).parent.absolute(),
-    BRANCH_TO_TEST=BRANCH_TO_TEST,
-    CHECK_MSG=CHECK_MSG,
+    BRANCH_TO_TEST="master",
+    CHECK_MSG="Welcome to your CakePHP",
 )
-
-IMAGE_TAG = f"mysql:{VARS.MYSQL_VERSION}"
 
 
 def skip_clear_env_tests():
